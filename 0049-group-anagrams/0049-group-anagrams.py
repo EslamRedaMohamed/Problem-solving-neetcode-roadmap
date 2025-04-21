fb5d405f017ad8032eac49216cbc44808f5fdc6c
Eslam Reda-إسلام رضا <104031370+EslamRedaMohamed@users.noreply.github.com>
2025-04-21 18:18:46 +0200

    Time: 12 ms (60.91%), Space: 21 MB (50.68%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        memo = {}
+        anagrams = {}
 
         for str in strs:
             str_sorted_tuple = tuple(sorted(str))
 
-            if str_sorted_tuple in memo:
-                memo[str_sorted_tuple].append(str)
+            if str_sorted_tuple in anagrams:
+                anagrams[str_sorted_tuple].append(str)
             else:
-                memo[str_sorted_tuple] = [str]
+                anagrams[str_sorted_tuple] = [str]
 
-        return list(memo.values())
+        return list(anagrams.values())
